62ea669b9f1cb13edb7419b394f6fa7ed82c51c3
秋雨 <574277409@qq.com>
2018-05-16 20:32:37 +0800

    图片

---
+++ images/config.js
@@ -0,0 +1,14 @@
+const imgConfig = {
+    background: 'images/bg_day.png',
+    ground: 'images/ground.png',
+    //
+    pipe_up: 'images/pipe_up.png'
+    pipe_down: 'images/pipe_down.png'
+    // title: 'images/name.png',
+    // start: 'images/start.png',
+    // sceneEnd: 'images/gameOver.png',
+    //
+    palyer_idle1: 'images/bird1_0.png',
+    palyer_idle2: 'images/bird1_1.png',
+    palyer_idle3: 'images/bird1_2.png',
+}
